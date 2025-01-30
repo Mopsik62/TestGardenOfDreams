844e18dba8cccfe1117710365fbc37b08c2898cd
Mopsik62 <74981499+Mopsik62@users.noreply.github.com>
2025-01-30 13:27:16 +0300

    Simple damage dealing

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class Enemy : Fighter
 {
     [SerializeField] private GameObject Loot;
-    private void Awake()
+    public int contactDamage;
+    protected override void Awake()
     {
+        base.Awake();
         HealthBar.SetMaxHealth(MaxHp);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag("Player"))
+        {
+            Damage dmg = new()
+            {
+                damage = contactDamage,
+            };
+            coll.gameObject.SendMessage("ReciveDamage", dmg);
+        }
     }
 
     protected override void Death()
--- Assets/Scripts/Fighter.cs
+++ Assets/Scripts/Fighter.cs
@@ -7,11 +7,35 @@ public class Fighter : MonoBehaviour
     public int MaxHp;
     public int CurHp;
 
+    public float immuneTime = 1f;
+    protected float lastImmune;
+
+    protected virtual void Awake()
+    {
+        lastImmune = Time.time;
+    }
+
     public HealthBar HealthBar;
     public virtual void Initialize()
     {
         CurHp = MaxHp;
         HealthBar.SetMaxHealth(MaxHp);
+        HealthBar.SetHealth(MaxHp);
+    }
+
+    public void ReciveDamage(Damage dmg)
+    {
+        if (Time.time - lastImmune > immuneTime)
+        {
+            lastImmune = Time.time;
+            CurHp -= dmg.damage;
+            HealthBar.SetHealth(CurHp);
+            if (CurHp <= 0)
+            {
+                Death();
+            }
+        }
+
     }
 
     protected virtual void Death()
